526216723c3f936b7b4a2fa36f13c240741e1d56
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-02-03 16:46:33 +0100

    Added diagnostics.go with placeholder output

---
+++ http_server/diagnostics.go
@@ -0,0 +1,24 @@
+package http_server
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func DiagnosticHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+
+	output := "{\n"
+	output += fmt.Sprintf("\"gutendexapi\": \"%s\",\n", http.StatusText(http.StatusBadGateway))
+	output += fmt.Sprintf("\"languageapi\": \"%s\",\n", http.StatusText(http.StatusBadGateway))
+	output += fmt.Sprintf("\"countriesapi\": \"%s\",\n", http.StatusText(http.StatusBadGateway))
+	output += fmt.Sprintf("\"version\": \"%s\",\n", "v1.0.0")
+	output += fmt.Sprintf("\"uptime\": \"%f\",\n", time.Since(StartTime).Seconds())
+	output += "}"
+
+	_, err := fmt.Fprintf(w, "%v", output)
+	if err != nil {
+		http.Error(w, "Error when returning output.", http.StatusInternalServerError)
+	}
+}
