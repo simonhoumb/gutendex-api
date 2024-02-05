57f7001e5b22e569390bf7361f4b397db435b5a1
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-02-05 10:47:35 +0100

    Added constants.go for url paths

---
+++ http_server/constants.go
@@ -0,0 +1,4 @@
+package http_server
+
+const DEFAULT_PATH = "/"
+const STATUS_PATH = "/status"
--- http_server/server.go
+++ http_server/server.go
@@ -11,13 +11,12 @@ var StartTime time.Time
 
 func Start() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		log.Println("$PORT has not been set. Defaulting to 8080")
 		port = "8080"
 	}
 
-	http.HandleFunc("/status", DiagnosticHandler)
+	http.HandleFunc(STATUS_PATH, DiagnosticHandler)
 
 	log.Println("Starting http_server on port " + port + "...")
 	StartTime = time.Now()
