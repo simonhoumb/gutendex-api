96d011ccb781cfb5272418002eaac8e123e38419
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-02-06 15:43:34 +0100

    Added resource urls

--- http_server/constants.go
+++ http_server/constants.go
@@ -1,6 +1,15 @@
 package http_server
 
-const DEFAULT_PATH = "/librarystats/v1/"
-const BOOKCOUNT_PATH = "/librarystats/v1/bookcount/"
-const READERSHIP_PATH = "/librarystats/v1/readership/"
-const STATUS_PATH = "/librarystats/v1/status"
+const VERSION = "v1"
+
+// http url paths
+const EMPTY_PATH = "/"
+const DEFAULT_PATH = "/librarystats/" + VERSION + "/"
+const BOOKCOUNT_PATH = "/librarystats/" + VERSION + "/bookcount/"
+const READERSHIP_PATH = "/librarystats/" + VERSION + "/readership/"
+const STATUS_PATH = "/librarystats/" + VERSION + "/status/"
+
+// resource urls
+const GUTENDEXAPI_URL = "http://129.241.150.113:8000/books/"
+const LANGUAGEAPI_URL = "http://129.241.150.113:3000/language2countries/"
+const COUNTRIESAPI_URL = "http://129.241.150.113:8080/v3.1"
