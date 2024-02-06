09ae9d73fe179351ebe8129a623db9eb3bf0ec34
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-02-06 23:06:27 +0100

    Added check for language code and limit parameter in url

--- http_server/readership.go
+++ http_server/readership.go
@@ -1,7 +1,10 @@
 package http_server
 
 import (
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type Readership struct {
@@ -16,14 +19,38 @@ func ReadershipHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		w.Header().Add("Content-Type", "application/json")
 
-		readershipOutput := Readership{
-			Country:    "Norway",
-			IsoCode:    "NO",
-			Books:      211,
-			Authors:    14,
-			Readership: 5400000 / 211}
+		path := r.URL.Path
+		parts := strings.Split(path, "/")
 
-		encodeJSON(w, readershipOutput)
+		var languageCode string
+
+		if len(parts) >= 5 {
+			languageCode = parts[4]
+		}
+
+		limit, err := strconv.Atoi(r.URL.Query().Get("limit"))
+		if err != nil {
+			log.Println("Value of parameter 'limit' is not valid: ", err.Error())
+		}
+
+		if limit == 0 {
+			limit = 1
+		}
+
+		if languageCode != "" && len(languageCode) == 2 {
+			for i := 0; i < limit; i++ {
+				readershipOutput := Readership{
+					Country:    "Norway",
+					IsoCode:    languageCode,
+					Books:      211,
+					Authors:    14,
+					Readership: 5400000 / 211}
+
+				encodeJSON(w, readershipOutput)
+			}
+		} else {
+			http.Error(w, "No language code provided.", http.StatusBadRequest)
+		}
 
 	}
 }
