08892ecb7f5782338ae916a5c7b0267c86462b84
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-02-17 01:41:56 +0100

    Added fallback url and default port

--- http_server/constants.go
+++ http_server/constants.go
@@ -1,5 +1,6 @@
 package http_server
 
+const DEFAULT_PORT = "8080"
 const VERSION = "v1"
 
 // http url paths
@@ -11,5 +12,8 @@ const STATUS_PATH = "/librarystats/" + VERSION + "/status/"
 
 // resource urls
 const GUTENDEXAPI_URL = "http://129.241.150.113:8000/books/"
+const FALLBACK_GUTENDEXAPI_URL = "https://gutendex.com/"
 const LANGUAGEAPI_URL = "http://129.241.150.113:3000/language2countries/"
+const FALLBACK_LANGUAGEAPI_URL = "https://language2countries.onrender.com/"
 const COUNTRIESAPI_URL = "http://129.241.150.113:8080/v3.1"
+const FALLBACK_COUNTRIESAPI_URL = "https://restcountries.com/"
