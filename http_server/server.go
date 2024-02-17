3d24db6287562571a2114609a86766956359ee9e
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-02-17 01:42:18 +0100

    Added decoder method and global client

--- http_server/server.go
+++ http_server/server.go
@@ -2,6 +2,7 @@ package http_server
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -9,14 +10,17 @@ import (
 )
 
 var StartTime time.Time
+var httpClient = &http.Client{Timeout: 3 * time.Second}
 
 func Start() {
+	defer httpClient.CloseIdleConnections()
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Println("$PORT has not been set. Defaulting to 8080")
-		port = "8080"
+		port = DEFAULT_PORT
 	}
 
+	http.HandleFunc(ROOT_PATH, RootHandler)
 	http.HandleFunc(STATUS_PATH, StatusHandler)
 	http.HandleFunc(READERSHIP_PATH, ReadershipHandler)
 	http.HandleFunc(BOOKCOUNT_PATH, BookCountHandler)
@@ -32,5 +36,15 @@ func encodeJSON(w http.ResponseWriter, v any) {
 	if err != nil {
 		log.Println("Error during JSON encoding: ", err.Error())
 		http.Error(w, "Error during JSON encoding.", http.StatusInternalServerError)
+	}
+}
+
+func decodeJSON(w http.ResponseWriter, res *http.Response, v any) {
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&v); err == io.EOF {
+		return
+	} else if err != nil {
+		log.Println("Error during JSON decoding: ", err.Error())
+		http.Error(w, "Error during JSON decoding.", http.StatusInternalServerError)
 	}
 }
