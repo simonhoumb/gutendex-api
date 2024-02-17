6db9bc8c8f80c9d0aae151cf59568f32b8ac8662
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-02-17 01:40:36 +0100

    Added gutendex structs

---
+++ api/gutendex/gutendex.go
@@ -0,0 +1,27 @@
+package gutendex
+
+type Books struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []Book `json:"results"`
+}
+
+type Book struct {
+	Id            int      `json:"id"`
+	Title         string   `json:"title"`
+	Subjects      []string `json:"subjects"`
+	Authors       []Person `json:"authors"`
+	Translators   []Person `json:"translators"`
+	Bookshelves   []string `json:"bookshelves"`
+	Languages     []string `json:"languages"`
+	Copyright     bool     `json:"copyright"`
+	MediaType     string   `json:"media_type"`
+	DownloadCount int      `json:"download_count"`
+}
+
+type Person struct {
+	BirthYear int    `json:"birth_year"`
+	DeathYear int    `json:"death_year"`
+	Name      string `json:"name"`
+}
