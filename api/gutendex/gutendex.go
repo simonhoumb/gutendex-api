ebe7fb5781b2c7e813ae232b6061eb7c5e01f057
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-03-07 12:44:35 +0100

    Removed fields from struct

--- api/gutendex/gutendex.go
+++ api/gutendex/gutendex.go
@@ -4,20 +4,14 @@ type Books struct {
 	Count    int     `json:"count"`
 	Next     *string `json:"next"`
 	Previous string  `json:"previous"`
-	Results  []Book  `json:"results"`
+	Results  *[]Book `json:"results"`
 }
 
 type Book struct {
-	Id            int      `json:"id"`
-	Title         string   `json:"title"`
-	Subjects      []string `json:"subjects"`
-	Authors       []Person `json:"authors"`
-	Translators   []Person `json:"translators"`
-	Bookshelves   []string `json:"bookshelves"`
-	Languages     []string `json:"languages"`
-	Copyright     bool     `json:"copyright"`
-	MediaType     string   `json:"media_type"`
-	DownloadCount int      `json:"download_count"`
+	Id        int      `json:"id"`
+	Title     string   `json:"title"`
+	Authors   []Person `json:"authors"`
+	Languages []string `json:"languages"`
 }
 
 type Person struct {
